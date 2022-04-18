7ab2c676f1a699586dbe3dcdf3deab3485d028ad
eyupkureli <eyupkurelii@gmail.com>
2022-04-18 10:04:01 +0100

    testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,53 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { render, unmountComponentAtNode } from 'react-dom';
+import { act } from 'react-dom/test-utils';
+import Trains from '../src/container/Trains';
 
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+let container = null;
+beforeEach(() => {
+  // setup a DOM element as a render target
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  // cleanup on exiting
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+
+describe('Main', () => {
+    it('render activity data', async () => {
+        const fakeTrain = {
+            'subscriberCount': 'Train',
+            'title': 'title',
+            'id': 1,
+        };
+
+        jest.spyOn(global, 'fetch').mockImplementation(() => 
+            Promise.resolve({
+                json: () => Promise.resolve(fakeTrain)
+            })
+        );
+
+        await act(async () => {
+            render(<Trains />, container);
+        });
+
+        expect(container.querySelector('[data-test="train-subscriberCount"]').textContent).toBe('Train');
+        expect(container.querySelector('[data-test="train-title"]').textContent).toBe('title');
+        expect(container.querySelector('[data-test="train-id"]').textContent).toBe('1');
+  
+
+        // remove the mock to ensure tests are completely isolated
+        global.fetch.mockRestore();
+    });
 });
